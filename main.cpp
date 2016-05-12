f3cf4ede9b209430283a65d138b25b2e5e3276df
Martin <thalira@gmx.de>
2016-05-12 13:26:14 +0200

    a new file...
    A smal c/c++ example...

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+  // cout << "Hello World!" << endl;
+  
+  int x = 0;
+  
+  for(x 0 1; x <= 25; x++)
+  {
+    if(x == 25)
+    {
+      cout << x;
+      break;
+    }
+  
+    cout << x << ", ";
+  
+  }
+
+cout << endl;
+
+}
